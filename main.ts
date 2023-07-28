8d542d2fe55ae7258ead85ba10ba5b49b9dca5af
Floron <floron@126.com>
2023-07-28 20:31:25 +0800

    展开收起基本逻辑完成，准备添加节流函数减少性能损耗

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
+import { log } from 'console';
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { env } from 'process';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	openLeftSetting: boolean;
+	openRightSetting: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	openLeftSetting: true,
+	openRightSetting: true
+}
+const ExpandAutoStatus = {
+	leftIsAutoOpen: false,
+	rightIsAutoOpen: false
 }
 
 export default class MyPlugin extends Plugin {
@@ -16,62 +24,38 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerDomEvent(document, 'mousemove', (evt: MouseEvent) => {
+			// console.log('mousemove', evt.pageX);
+			let x = evt.pageX;
+			let leftBorder = 40;
+			let leftSidebarWidth = 334;
+			let rightBorder = 1480;
+
+			if (x < leftBorder) {
+				console.log('左侧展开')
+				// TODO 将侧边栏展开
+				ExpandAutoStatus.leftIsAutoOpen = true;
+			}
+			if (x > leftSidebarWidth && ExpandAutoStatus.leftIsAutoOpen) {
+				// TODO 将侧边栏收起
+				console.log('左侧收起')
+				ExpandAutoStatus.leftIsAutoOpen = false;
+			}
+			if (x > rightBorder) {
+				console.log('右侧展开')
+				ExpandAutoStatus.leftIsAutoOpen = true;
+			}
+			if (x > leftSidebarWidth && ExpandAutoStatus.leftIsAutoOpen) {
+				// TODO 将侧边栏收起
+				console.log('右侧收起')
+				ExpandAutoStatus.leftIsAutoOpen = false;
+			}
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
@@ -91,18 +75,32 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
+function throttle<T extends (...args: any[]) => void>(callback: T, delay: number): T {
+	let timerId: ReturnType<typeof setTimeout> | null;
+
+	return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+		if (!timerId) {
+			timerId = setTimeout(() => {
+				callback.apply(this, args);
+				timerId = null;
+			}, delay);
+		}
+	} as T;
+}
+
+
 class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,19 +114,30 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('自动展开左侧边栏')
+			.setDesc('')
+			.addButton(val => val.onClick(async (isOpen) => {
+				this.plugin.settings.openLeftSetting = false
+			}));
+
+		new Setting(containerEl)
+			.setName('自动展开右侧边栏')
+			.setDesc('')
+			.addButton(val => val.onClick(async (isOpen) => {
+				this.plugin.settings.openRightSetting = false
+			}));
+
+		// .addText(text => text
+		// 	.setPlaceholder('Enter your secret')
+		// 	.setValue(this.plugin.settings.mySetting)
+		// 	.onChange(async (value) => {
+		// 		this.plugin.settings.mySetting = value;
+		// 		await this.plugin.saveSettings();
+		// 	}));
 	}
 }
